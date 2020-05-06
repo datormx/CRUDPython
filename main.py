f7212d0dd231179cf520bf14cd5ba8f837608313
Miguel Torres <migueltorresio@outlook.com>
2020-05-06 18:47:21 -0500

    Se inicia el proyecto CRUD del curso

---
+++ main.py
@@ -0,0 +1,27 @@
+clients = 'pablo,ricardo,'
+
+def create_client(client_name):
+    global clients
+
+    clients += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+
+if __name__ == "__main__":
+    list_clients()
+
+    create_client('David')
+    
+    list_clients()
