b2a0053e0f2dd8b8d59acabda1dbb6b28d1f856e
Miguel Torres <migueltorresio@outlook.com>
2020-05-07 10:55:52 -0500

    Tercera sesión de clases: Se agregan la función _print_welcome y el input para que el usuario decida si crear o borrar un cliente

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ clients = 'pablo,ricardo,'
 def create_client(client_name):
     global clients
 
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    elif client_name in clients:
+        print('Client already is in the client\'s list')
 
 
 def list_clients():
@@ -19,9 +22,24 @@ def _add_comma():
     clients += ','
 
 
-if __name__ == "__main__":
-    list_clients()
+def _print_welcome():
+    print('WELCOME TO PLATZI VENTAS')
+    print('*' * 50)
+    print('What would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client')
 
-    create_client('David')
-    
-    list_clients()
+
+if __name__ == "__main__":
+
+    _print_welcome()
+
+    command = input()
+
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        print('Invalid command')
+        list_clients()
