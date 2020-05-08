cb8bd09bae9a088f2b2325da8303283a5d3db07e
Miguel Torres <migueltorresio@outlook.com>
2020-05-08 15:16:09 -0500

    Cuarta sesión de clases: Se agregan las funciones Update, Delete, Search y _get_client_name

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
 clients = 'pablo,ricardo,'
+
 
 def create_client(client_name):
     global clients
-
     if client_name not in clients:
+
         clients += client_name
         _add_comma()
     elif client_name in clients:
@@ -14,6 +16,36 @@ def list_clients():
     global clients
 
     print(clients)
+
+
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+        list_clients()    
+    else:
+        print('Client is not in clients list')
+
+
+def delete_client(client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+        list_clients()
+    else:
+        print('Client is not in clients list')
+
+
+def search_client(client_name):
+    clients_list = clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        elif client == client_name:
+            return True
 
 
 def _add_comma():
@@ -27,7 +59,26 @@ def _print_welcome():
     print('*' * 50)
     print('What would you like to do today?')
     print('[C]reate client')
+    print('[R]ead clients list')
+    print('[U]pdate client')
     print('[D]elete client')
+    print('[S]earch client')
+
+
+def _get_client_name():
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')  
+
+        if client_name == 'exit':
+            client_name = None
+            break 
+
+    if not client_name: 
+        sys.exit()
+                
+    return client_name
 
 
 if __name__ == "__main__":
@@ -35,11 +86,28 @@ if __name__ == "__main__":
     _print_welcome()
 
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
-    elif command == 'D':
-        print('Invalid command')
+    elif command == 'R':
         list_clients()
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name? ') 
+        update_client(client_name, updated_client_name)
+    elif command == 'D':
+        client_name = _get_client_name()
+        delete_client(client_name)
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+        
+        if found:
+            print('The client is in client\'s list')
+        else:
+            print(f'The client {client_name} was not found')
+    else:
+        print('Invalid command')
