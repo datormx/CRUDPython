4847382e8d1e33d7df04dd39c9d987e3c57c5a44
Miguel Torres <migueltorresio@outlook.com>
2020-05-11 12:58:32 -0500

    Sexta sesión: Se agrega manejo de métodos por diccionario clients. Ahora no solo se almacenan los nombres de clientes. Se agrega funcion _get_client_from_user para ser obtener junto con _get_client_field los valores del objeto diccionario client para Create y Update

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import sys
-clients = ['pablo', 'ricardo']
+
+clients = [
+    {
+        'name': 'Pablo',
+        'company': 'Google',
+        'email': 'pablo@google.com',
+        'position': 'Software Engineer',
+    },
+    {
+        'name': 'Ricardo',
+        'company': 'Facebook',
+        'email': 'ricardo@facebook.com',
+        'position': 'Data Engineer',
+    }
+]
 
 
-def create_client(client_name):
+def create_client(client):
     global clients
-    if client_name not in clients:
-        clients.append(client_name)               
-    elif client_name in clients:
+    if client not in clients:
+        clients.append(client)               
+    elif client in clients:
         print('Client already is in the client\'s list')
 
 
@@ -14,37 +28,56 @@ def list_clients():
     global clients
 
     for idx, client in enumerate(clients):
-        print(f'{idx}: {client}')
+        print('{uid} | {name} | {company} | {email} | {position}'.format(
+            uid = idx,
+            name = client['name'],
+            company = client['company'],
+            email = client['email'],
+            position = client['position']))
 
 
-def update_client(client_name, updated_client_name):
+def update_client(client_name, updated_client):
     global clients
 
-    if client_name in clients:
-        index = clients.index(client_name)
-        clients[index] = updated_client_name
-        list_clients()    
-    else:
-        print('Client is not in clients list')
+    for idx, client in enumerate(clients):
+        if client['name'] == client_name:
+            clients[idx] = updated_client
+            list_clients()    
 
 
-def delete_client(client_name):
+def delete_client(client_name): #Returns found to validate in main execution if the clients exists in list
     global clients
+    found = None
 
-    if client_name in clients:
-        clients.remove(client_name)
-        list_clients()
-    else:
-        print('Client is not in clients list')
+    for idx, client in enumerate(clients):
+        if client['name'] == client_name:
+            clients.pop(idx)
+            list_clients() 
+            found = True
+        else:
+            found = False
+
+    return found            
 
 
 def search_client(client_name):
-    
+    global clients
+    found = None
+
     for client in clients:
-        if client != client_name:
+        if client['name'] != client_name:
+            found = False
             continue
-        elif client == client_name:
-            return True
+        elif client['name'] == client_name:
+            found = True
+            break
+
+    if found:
+        print('The client is in client\'s list')
+    else:
+        print(f'The client {client_name} was not found')
+
+    return found
 
 
 def _print_welcome():
@@ -57,21 +90,40 @@ def _print_welcome():
     print('[D]elete client')
     print('[S]earch client')
 
+def _get_client_from_user():
+    client = {
+            'name': _get_client_field('name'),
+            'company': _get_client_field('company'),
+            'email': _get_client_field('email'),
+            'position': _get_client_field('position'),         
+            }
 
-def _get_client_name():
-    client_name = None
+    return client
+    
 
-    while not client_name:
-        client_name = input('What is the client name? ')  
+def _get_client_field(field_name):
+    field = None
 
-        if client_name == 'exit':
-            client_name = None
-            break 
+    while not field:
+        field = input(f'What is the client {field_name}? ')
+    
+    return field
 
-    if not client_name: 
-        sys.exit()
+
+# def _get_client_name():
+#     client_name = None
+
+#     while not client_name:
+#         client_name = input('What is the client name? ')  
+
+#         if client_name == 'exit':
+#             client_name = None
+#             break 
+
+#     if not client_name: 
+#         sys.exit()
                 
-    return client_name
+#     return client_name
 
 
 if __name__ == "__main__":
@@ -82,25 +134,28 @@ if __name__ == "__main__":
     command = command.upper()
 
     if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
+        client = _get_client_from_user()
+        create_client(client)
         list_clients()
     elif command == 'R':
         list_clients()
     elif command == 'U':
-        client_name = _get_client_name()
-        updated_client_name = input('What is the updated client name? ') 
-        update_client(client_name, updated_client_name)
+        client_name = _get_client_field('name')
+        validate = search_client(client_name)  #Using search method to validate if client exists in lists
+
+        if validate:   
+            print(f'\nIntroduce the new values for the client you want to update:')
+            updated_client = _get_client_from_user()
+            update_client(client_name, updated_client)
     elif command == 'D':
-        client_name = _get_client_name()
-        delete_client(client_name)
+        client_name = _get_client_field('name')
+        validate = delete_client(client_name) #This method deletes the client if exists in list, if not returns False to validate
+
+        if validate == False: 
+            print(f'Client {client_name} is not in clients list')
     elif command == 'S':
-        client_name = _get_client_name()
-        found = search_client(client_name)
+        client_name = _get_client_field('name')
+        search_client(client_name)      
         
-        if found:
-            print('The client is in client\'s list')
-        else:
-            print(f'The client {client_name} was not found')
     else:
         print('Invalid command')
